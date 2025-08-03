45d2d7642351341e4fe12961ba44011cdf19f035
karina <jmarchel100@gmail.com>
2025-08-03 23:16:01 +0700

    models

--- backend/src/models.rs
+++ backend/src/models.rs
@@ -1,10 +1,11 @@
+use axum::{http::StatusCode, response::IntoResponse};
 use chrono::{DateTime, Utc};
 use serde::{Deserialize, Serialize};
 use sqlx::prelude::FromRow;
 use uuid::Uuid;
 use validator::Validate;
 
-#[derive(Debug, FromRow)]
+#[derive(Debug, FromRow, Deserialize)]
 pub struct User {
     pub id: Uuid,
     pub username: Option<String>,
@@ -24,7 +25,22 @@ pub struct RegisterPayload {
     pub email: String,
 }
 
-#[derive(Serialize)]
-pub struct GenericResponse {
-    pub message: String,
+#[derive(Debug)]
+pub enum AppResponse {
+    Message(String),
+    Created(String),
+    Redirect(String),
+}
+
+impl IntoResponse for AppResponse {
+    fn into_response(self) -> axum::response::Response {
+        let (status, message) = match self {
+            AppResponse::Message(m) => (StatusCode::OK, format!("{m}")),
+            AppResponse::Created(m) => (StatusCode::CREATED, format!("{m}")),
+            AppResponse::Redirect(m) => (StatusCode::OK, format!("{m}")),
+        };
+
+        let body = serde_json::json!({"message": message});
+        (status, axum::Json(body)).into_response()
+    }
 }
