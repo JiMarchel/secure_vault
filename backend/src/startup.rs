f30abd7be8cb56348528c7791536d6992c220519
karina <jmarchel100@gmail.com>
2025-08-06 23:53:39 +0700

    startup

--- backend/src/startup.rs
+++ backend/src/startup.rs
@@ -8,7 +8,7 @@ use axum::{
 use sqlx::{Pool, Postgres};
 use tokio::net::TcpListener;
 
-use crate::routes::{health_check::health_check, sign_up::sign_up};
+use crate::routes::{get_user_basic_info::get_user_basic_info, health_check::health_check, sign_up::sign_up};
 
 pub struct ApplicationState {
     pub pool: Pool<Postgres>,
@@ -19,6 +19,7 @@ pub async fn run(listener: TcpListener, pool: Pool<Postgres>) {
 
     let app = Router::new()
         .route("/health_check", get(health_check))
+        .route("/users/basic/{id}", get(get_user_basic_info))
         .route("/sign-up", post(sign_up))
         .with_state(app_state);
 
