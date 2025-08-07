85d2160e1db0cbcb666eb7604787277716d8a0ff
karina <jmarchel100@gmail.com>
2025-08-07 23:46:40 +0700

    verif otp schema

--- frontend/lib/schemas/verif-otp.ts
+++ frontend/lib/schemas/verif-otp.ts
@@ -1,5 +1,6 @@
 import z from "zod";
 
 export const otpVerifSchema = z.object({
-  otp: z.string().length(6, "OTP must be exactly 6 digits long."),
+  otp_code: z.string().length(6, "OTP must be exactly 6 digits long."),
+  id: z.uuid(),
 });
