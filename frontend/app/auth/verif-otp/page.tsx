775b8f30c19e0e5c04b3d72aaf57465d592e8f48
karina <jmarchel100@gmail.com>
2025-08-07 23:45:33 +0700

    page verif otp

--- frontend/app/auth/verif-otp/page.tsx
+++ frontend/app/auth/verif-otp/page.tsx
@@ -1,12 +1,17 @@
 import { getUserBasicInfo } from "@/lib/query/get-user-basic-info";
 import { UserInfo } from "./user-info";
 import { cookies } from "next/headers";
+import { getOtpCode } from "@/lib/query/get-otp-code";
 
 const AuthVerifOtp = async () => {
   const cookie = cookies();
   const userId = (await cookie).get("sc-verif-otp")?.value;
-  const getUser = await getUserBasicInfo(userId);
-  return <UserInfo />;
+  const user = await getUserBasicInfo(userId);
+  const otpCode = await getOtpCode(userId);
+
+  console.log(otpCode);
+
+  return <UserInfo username={user.username} email={user.email} otpExpiresAt={otpCode.otp_expires_at} id={userId}/>;
 };
 
 export default AuthVerifOtp;
