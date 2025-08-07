fd403d0f38e5597ffba9b5a589467b9a427ae9c7
karina <jmarchel100@gmail.com>
2025-08-07 23:44:48 +0700

    modif

--- backend/src/helper/fetch_user_basic_info.rs
+++ backend/src/helper/fetch_user_basic_info.rs
@@ -2,6 +2,8 @@ use serde::Serialize;
 use sqlx::{Pool, Postgres};
 use uuid::Uuid;
 
+use crate::{error::AppError};
+
 #[derive(Serialize)]
 pub struct UserBasicInfo {
     pub id: Uuid,
@@ -11,13 +13,21 @@ pub struct UserBasicInfo {
 
 pub async fn fetch_user_basic_info(
     pool: &Pool<Postgres>,
-    id: Uuid,
-) -> Result<Option<UserBasicInfo>, sqlx::Error> {
-    sqlx::query_as!(
+    id: &str,
+) -> Result<Option<UserBasicInfo>, AppError> {
+    let valid_id = Uuid::parse_str(id).map_err(|_| AppError::BadRequest("Invalid Uuid format".into()))?;
+
+    let maybe_user = sqlx::query_as!(
         UserBasicInfo,
         "SELECT id, username, email FROM users WHERE id = $1",
-        id
+        valid_id
     )
     .fetch_optional(pool)
-    .await
+    .await?;
+
+    if let Some(user) = maybe_user {
+        Ok(Some(user))
+    } else {
+        Err(AppError::NotFound)
+    }
 }
