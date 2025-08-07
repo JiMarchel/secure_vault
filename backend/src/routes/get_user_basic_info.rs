fca6e0ca1257883c22ccf4b109db2f34beca59e1
karina <jmarchel100@gmail.com>
2025-08-07 23:44:51 +0700

    modif

--- backend/src/routes/get_user_basic_info.rs
+++ backend/src/routes/get_user_basic_info.rs
@@ -4,18 +4,18 @@ use axum::{
     Json,
     extract::{Path, State},
 };
-use uuid::Uuid;
 
 use crate::{
-    error::AppError, helper::fetch_user_basic_info::{fetch_user_basic_info, UserBasicInfo}, 
+    error::AppError,
+    helper::fetch_user_basic_info::{UserBasicInfo, fetch_user_basic_info},
     startup::ApplicationState,
 };
 
 pub async fn get_user_basic_info(
     State(app_state): State<Arc<ApplicationState>>,
-    Path(user_id): Path<Uuid>,
+    Path(user_id): Path<String>,
 ) -> Result<Json<UserBasicInfo>, AppError> {
-    let maybe_user = fetch_user_basic_info(&app_state.pool, user_id).await?;
+    let maybe_user = fetch_user_basic_info(&app_state.pool, &user_id).await?;
 
     if let Some(user) = maybe_user {
         Ok(Json(user))
