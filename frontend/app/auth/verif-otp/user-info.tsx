34772ec7c398c55f3af4a9fcdcbb74f8e083910a
karina <jmarchel100@gmail.com>
2025-08-07 23:45:44 +0700

    component verif otp

--- frontend/app/auth/verif-otp/user-info.tsx
+++ frontend/app/auth/verif-otp/user-info.tsx
@@ -1,4 +1,5 @@
 "use client";
+import { Badge } from "@/components/ui/badge";
 import { Button } from "@/components/ui/button";
 import {
   Card,
@@ -9,25 +10,50 @@ import {
   CardHeader,
   CardTitle,
 } from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
 import {
   InputOTP,
   InputOTPGroup,
   InputOTPSeparator,
   InputOTPSlot,
 } from "@/components/ui/input-otp";
+import useCountDown from "@/hooks/useCountDown";
+import { updateVerifOtp } from "@/lib/actions/update-verif-otp";
 import { verifOtpAction } from "@/lib/actions/verif-otp";
 import { OtpVerifActionResponse } from "@/lib/types";
 import { REGEXP_ONLY_DIGITS } from "input-otp";
-import {  use, useActionState } from "react";
+import { Mail } from "lucide-react";
+import { useActionState } from "react";
 
+let otpInitialvalue: OtpVerifActionResponse = {};
 
-let initialvalue: OtpVerifActionResponse = {};
+interface UserInfoProps {
+  id: string | undefined;
+  username: string;
+  email: string;
+  otpExpiresAt: string;
+}
 
-export const UserInfo = ({}) => {
-  const [state, formAction, pending] = useActionState(
+export const UserInfo = ({
+  username,
+  email,
+  otpExpiresAt,
+  id,
+}: UserInfoProps) => {
+  const [otpState, otpFormAction, otpPending] = useActionState(
     verifOtpAction,
-    initialvalue
+    otpInitialvalue
   );
+
+  const { countdown, start } = useCountDown();
+  const now = new Date();
+  const expiresAt = new Date(otpExpiresAt);
+  const difference = expiresAt.getTime() - now.getTime();
+
+  const handleResendAction = async (formData: FormData) => {
+    start(60);
+    await updateVerifOtp(null, formData);
+  };
 
   return (
     <div className="fixed top-1/2 left-1/2 -translate-y-1/2 -translate-x-1/2 w-xl bg-gray-50 shadow-2xl">
@@ -35,18 +61,36 @@ export const UserInfo = ({}) => {
         <CardHeader>
           <CardTitle className="text-2xl">Verify OTP</CardTitle>
           <CardDescription>
-            Please enter the OTP sent to your email, and please don't leave this
-            page until you have verified your account.
+            Hello <b className="text-primary">{username}</b>, please enter the
+            OTP sent to your email, and please don't leave this page until you
+            have verified your account.
           </CardDescription>
-          {/* <CardAction>Resend OTP</CardAction> */}
+          <CardAction>
+            <form action={handleResendAction}>
+              <Input
+                type="text"
+                defaultValue={id}
+                name="id"
+                className="hidden"
+                readOnly
+              />
+              <Button variant="link" type="submit" disabled={countdown > 0}>
+                Resend OTP {countdown > 0 ? `in ${countdown}` : null}
+              </Button>
+            </form>
+          </CardAction>
         </CardHeader>
-        <form action={formAction} className="space-y-6">
-          <CardContent className="flex justify-center flex-col items-center gap-2">
+        <form action={otpFormAction} className="space-y-6">
+          <CardContent className="flex justify-center flex-col items-center gap-4">
+            <Badge className="px-2 py-1">
+              <Mail /> {email}
+            </Badge>
+            <input name="id" value={id} className="hidden" readOnly/>
             <InputOTP
               maxLength={6}
               pattern={REGEXP_ONLY_DIGITS}
               name="otp"
-              defaultValue={state.inputs?.otp}
+              defaultValue={otpState.inputs?.otp_code}
             >
               <InputOTPGroup>
                 <InputOTPSlot index={0} />
@@ -60,19 +104,29 @@ export const UserInfo = ({}) => {
                 <InputOTPSlot index={5} />
               </InputOTPGroup>
             </InputOTP>
-            <span className="text-red-500 text-sm">{state.errors?.otp}</span>
+            <span className="text-red-500 text-sm">{otpState.errors?.otp_code}</span>
           </CardContent>
           <CardFooter className="flex flex-col justify-start items-start">
-            <span className="text-red-500">{state.messageApi}</span>
+            {difference < 0 ? (
+              <span className="text-red-500">
+                OTP expired, please request a new one
+              </span>
+            ) : (
+              <span className="text-emerald-500">
+                Your OTP is expired in 5 minutes
+              </span>
+            )}
+
+            <span className="text-red-500">{otpState.messageApi}</span>
             <span className="text-sm text-muted-foreground">
               Please enter the one-time password sent to your email.
             </span>
-            <Button type="submit">Submit</Button>
+            <Button type="submit" disabled={otpPending}>
+              Submit
+            </Button>
           </CardFooter>
         </form>
       </Card>
     </div>
   );
 };
-
-
