18841e7317ee9709cc53566039b66c279d67f7b0
jimarchel <jmarchel100@gmail.com>
2025-08-08 21:59:50 +0700

    feat: implement password verification action with validation

---
+++ frontend/lib/actions/verif-password.ts
@@ -0,0 +1,36 @@
+"use server";
+import { cookies } from "next/headers";
+import { VerifPasswordActionResponse } from "../types";
+import { verifPasswordSchema } from "../schemas/verif-password";
+import z from "zod";
+
+export async function verifPasswordAction(
+  _: VerifPasswordActionResponse,
+  formData: FormData
+): Promise<VerifPasswordActionResponse> {
+  const cookieStore = await cookies();
+  const rawData = {
+    password: formData.get("password") as string,
+    confirm_password: formData.get("confirm_password") as string,
+    id: cookieStore.get("sc-verif-password")?.value as string,
+  };
+
+  const validateFields = verifPasswordSchema.safeParse(rawData);
+  if (!validateFields.success) {
+    console.error(z.flattenError(validateFields.error).fieldErrors);
+    return {
+      errors: z.flattenError(validateFields.error).fieldErrors,
+      inputs: rawData,
+    };
+  }
+
+  console.log("Raw Data:", rawData);
+
+  const baseApiUrl = process.env.BASE_API_URL;
+
+  return {
+    // errors: {},
+    // inputs: data,
+    // messageApi: "Password verification successful.",
+  };
+}
