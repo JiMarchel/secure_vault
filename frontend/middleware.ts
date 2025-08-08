4d2d89d1180d68978869783bed26dbd3a1509ceb
jimarchel <jmarchel100@gmail.com>
2025-08-08 22:00:37 +0700

    feat: enhance middleware to handle verif-password cookie for redirection

--- frontend/middleware.ts
+++ frontend/middleware.ts
@@ -2,6 +2,8 @@ import { NextRequest, NextResponse } from "next/server";
 
 export function middleware(request: NextRequest) {
   const otpCookie = request.cookies.has("sc-verif-otp");
+  const verifPasswordCookie = request.cookies.has("sc-verif-password");
+
   const { pathname } = request.nextUrl;
 
   if (otpCookie && pathname !== "/auth/verif-otp") {
@@ -9,6 +11,14 @@ export function middleware(request: NextRequest) {
   }
 
   if (!otpCookie && pathname === "/auth/verif-otp") {
+    return NextResponse.redirect(new URL("/", request.url));
+  }
+
+  if (verifPasswordCookie && pathname !== "/auth/verif-password") {
+    return NextResponse.redirect(new URL("/auth/verif-password", request.url));
+  }
+
+  if (!verifPasswordCookie && pathname === "/auth/verif-password") {
     return NextResponse.redirect(new URL("/", request.url));
   }
 
