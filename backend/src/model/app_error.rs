a31fca418056ff5460e8948b54839a53227dcb4e
JiMarchel <jmarchel100@gmail.com>
2025-11-03 13:16:21 +0700

    feat: add AppError enum for error handling with custom error types

---
+++ backend/src/model/app_error.rs
@@ -0,0 +1,30 @@
+use thiserror::Error;
+
+#[derive(Debug, Error)]
+pub enum AppError {
+    #[error("Database error: {0}")]
+    Database(String),
+
+    #[error("Invalid credentials")]
+    InvalidCredentials,
+
+    #[error("Internal error: {0}")]
+    Internal(String),
+
+    #[error("Data Not Found")]
+    NotFound,
+
+    #[error("Bad Request: {0}")]
+    BadRequest(String),
+
+    #[error("Unauthorized")]
+    Unauthorized,
+
+    #[error("Conflict{0}")]
+    Conflict(String),
+
+    #[error("Forbidden: {0}")]
+    Forbidden(String),
+}
+
+pub type AppResult<T> = Result<T, AppError>;
