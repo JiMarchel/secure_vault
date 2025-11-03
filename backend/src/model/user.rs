d4e30a5b196ef20b9b69d36574b0923e11848487
JiMarchel <jmarchel100@gmail.com>
2025-11-03 13:16:27 +0700

    feat: implement user model with serialization and verification methods

---
+++ backend/src/model/user.rs
@@ -0,0 +1,35 @@
+use serde::Serialize;
+
+#[derive(Serialize)]
+pub struct SignUpResponse {
+    pub message: String,
+}
+
+#[derive(Serialize)]
+pub struct CheckSessionResponse {
+    pub authenticated: bool,
+    pub message: Option<String>,
+}
+
+#[derive(Serialize, sqlx::FromRow)]
+pub struct User {
+    pub id: uuid::Uuid,
+    pub username: String,
+    pub email: String,
+    pub encrypted_dek: Option<Vec<u8>>,
+    pub salt: Option<Vec<u8>>,
+    pub argon2_params: Option<String>,
+    pub is_email_verified: bool,
+    pub nonce: Option<Vec<u8>>,
+    pub created_at: chrono::NaiveDateTime,
+}
+
+impl User {
+    pub fn is_pending_password_verification(&self) -> bool {
+        self.is_email_verified && self.encrypted_dek.is_none()
+    }
+
+    pub fn is_pending_otp_verification(&self) -> bool {
+        !self.is_email_verified
+    }
+}
