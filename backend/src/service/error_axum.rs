043467699ae396e6296d2d050f6dc357dd66f14e
JiMarchel <jmarchel100@gmail.com>
2025-11-03 13:17:11 +0700

    feat: implement error handling for AppError in axum response

---
+++ backend/src/service/error_axum.rs
@@ -0,0 +1,29 @@
+use axum::{http::StatusCode, response::IntoResponse};
+
+use crate::model::app_error::AppError;
+
+impl IntoResponse for AppError {
+    fn into_response(self) -> axum::response::Response {
+        let (status, error_message) = match self {
+            AppError::Internal(_) => (
+                StatusCode::INTERNAL_SERVER_ERROR,
+                "Internal server error".to_string(),
+            ),
+            AppError::NotFound => (StatusCode::NOT_FOUND, "Data not found".to_string()),
+            AppError::BadRequest(msg) => (StatusCode::BAD_REQUEST, msg),
+            AppError::Unauthorized => (StatusCode::UNAUTHORIZED, "Unauthorized".to_string()),
+            AppError::Conflict(s) => (StatusCode::CONFLICT, s),
+            AppError::Forbidden(s) => (StatusCode::FORBIDDEN, s),
+            AppError::Database(_) => (
+                StatusCode::INTERNAL_SERVER_ERROR,
+                "Database error".to_string(),
+            ),
+            AppError::InvalidCredentials => {
+                (StatusCode::UNAUTHORIZED, "Invalid credentials".to_string())
+            }
+        };
+
+        let body = serde_json::json!({"error": error_message});
+        (status, axum::Json(body)).into_response()
+    }
+}
