5206264195a7bd00f73393370bc9d7c0ca070ed6
JiMarchel <jmarchel100@gmail.com>
2025-11-07 14:20:25 +0700

    feat: add OtpRecord struct for OTP handling with expiration time

--- backend/src/model/otp.rs
+++ backend/src/model/otp.rs
@@ -4,3 +4,9 @@ use serde::{Deserialize, Serialize};
 pub struct VerifyOtp {
     pub otp_code: String,
 }
+
+#[derive(Serialize, sqlx::FromRow)]
+pub struct OtpRecord {
+    pub otp_code: String,
+    pub otp_expires_at: chrono::DateTime<chrono::Utc>,
+}
