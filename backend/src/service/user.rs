f4a442d7a37964533df24278ee22a65f91c46352
JiMarchel <jmarchel100@gmail.com>
2025-11-07 14:20:59 +0700

    feat: add update_user_identifier method to UserPersistence for user data updates

--- backend/src/service/user.rs
+++ backend/src/service/user.rs
@@ -7,6 +7,14 @@ use crate::model::{app_error::AppResult, user::User};
 pub trait UserPersistence: Send + Sync {
     async fn create_user(&self, username: &str, email: &str) -> AppResult<Uuid>;
     async fn get_user_by_email(&self, email: &str) -> AppResult<Option<User>>;
-    async fn get_user_by_id(&self, id: Uuid)-> AppResult<Option<User>>;
+    async fn get_user_by_id(&self, id: Uuid) -> AppResult<Option<User>>;
     async fn update_email_verification(&self, id: Uuid) -> AppResult<()>;
+    async fn update_user_identifier(
+        &self,
+        encrypted_dek: String,
+        nonce: String,
+        salt: String,
+        argon2_params: String,
+        user_id: Uuid,
+    ) -> AppResult<()>;
 }
