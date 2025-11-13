f3a9428a246774bf14e20f5606d80cb927a72d25
JiMarchel <jmarchel100@gmail.com>
2025-11-13 17:54:52 +0700

    refactor: reorganize imports and enhance HTTP request tracing in create_app function

--- backend/src/infra/app.rs
+++ backend/src/infra/app.rs
@@ -1,24 +1,54 @@
 use axum::{
     Router,
-    http::{HeaderValue, Method, header::{ACCEPT, AUTHORIZATION, CONTENT_TYPE, SET_COOKIE}},
+    http::{
+        self, HeaderValue, Method,
+        header::{ACCEPT, AUTHORIZATION, CONTENT_TYPE, SET_COOKIE},
+    },
 };
-use tower_http::cors::CorsLayer;
+use tower_http::{
+    cors::CorsLayer,
+    trace::{TraceLayer},
+};
 use tower_sessions::SessionManagerLayer;
 use tower_sessions_sqlx_store::PostgresStore;
+use uuid::Uuid;
 
-use crate::controller::{self, app_state::AppState};
+use crate::{
+    controller::{self, app_state::AppState},
+    infra::setup::init_tracing,
+};
 
 pub fn create_app(app_state: AppState, session: SessionManagerLayer<PostgresStore>) -> Router {
+    init_tracing();
+
     let cors = CorsLayer::new()
         .allow_origin("http://localhost:3000".parse::<HeaderValue>().unwrap()) // 👈 React dev server
-        .allow_methods([Method::GET, Method::POST, Method::PUT, Method::DELETE, Method::OPTIONS])
+        .allow_methods([
+            Method::GET,
+            Method::POST,
+            Method::PUT,
+            Method::DELETE,
+            Method::OPTIONS,
+        ])
         .allow_headers([CONTENT_TYPE, ACCEPT, AUTHORIZATION])
         .expose_headers([SET_COOKIE])
         .allow_credentials(true);
 
     Router::new()
         .nest("/api", controller::router())
+        .with_state(app_state)
         .layer(cors)
         .layer(session)
-        .with_state(app_state)
+        .layer(
+            TraceLayer::new_for_http().make_span_with(|req: &http::Request<_>| {
+                let request_id = Uuid::new_v4();
+                tracing::info_span!(
+                    "http-request",
+                    method = %req.method(),
+                    uri = %req.uri(),
+                    version = ?req.version(),
+                    request_id = %request_id
+                )
+            }),
+        )
 }
