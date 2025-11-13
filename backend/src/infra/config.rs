2e1b325373c1abb7045b200d3577676e6d1e4728
JiMarchel <jmarchel100@gmail.com>
2025-11-13 17:54:56 +0700

    feat: add jwt_secret to AppConfig for enhanced security configuration

--- backend/src/infra/config.rs
+++ backend/src/infra/config.rs
@@ -6,6 +6,8 @@ pub struct AppConfig {
     pub smtp_username: String,
     pub smtp_password: String,
     pub smtp_from_email: String,
+
+    pub jwt_secret: String,
 }
 
 impl AppConfig {
@@ -14,12 +16,14 @@ impl AppConfig {
         let smtp_username = env::var("SMTP_USERNAME").expect("SMTP_USERNAME");
         let smtp_password = env::var("SMTP_PASSWORD").expect("SMTP_PASSWORD");
         let smtp_from_email = env::var("SMTP_FROM_EMAIL").expect("SMTP_FROM_EMAIL");
+        let jwt_secret = env::var("JWT_SECRET").expect("JWT_SECRET");
 
         Self {
             smtp_host,
             smtp_username,
             smtp_password,
             smtp_from_email,
+            jwt_secret,
         }
     }
 }
