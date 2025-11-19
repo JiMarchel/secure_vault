138c36569e6c42861d1e0e102d85192b57e14756
JiMarchel <jmarchel100@gmail.com>
2025-11-19 22:25:24 +0700

    refactor: rename VerifyOtp struct to VerifyOtpPayload for clarity

--- backend/src/model/otp.rs
+++ backend/src/model/otp.rs
@@ -1,11 +1,12 @@
 use serde::{Deserialize, Serialize};
 
 #[derive(Serialize, Deserialize)]
-pub struct VerifyOtp {
+pub struct VerifyOtpPayload {
     pub otp_code: String,
 }
 
 #[derive(Serialize, sqlx::FromRow)]
+#[serde(rename_all = "camelCase")]
 pub struct OtpRecord {
     pub otp_code: String,
     pub otp_expires_at: chrono::DateTime<chrono::Utc>,
