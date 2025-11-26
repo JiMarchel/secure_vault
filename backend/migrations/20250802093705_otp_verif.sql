a4de94a8c30e121d54961ad69c9c1ed5a6c1aff0
JiMarchel <jmarchel100@gmail.com>
2025-11-26 10:05:49 +0700

    chore: remove obsolete OTP verification migration script to clean up project structure

--- backend/migrations/20250802093705_otp_verif.sql
+++
@@ -1,6 +0,0 @@
--- Add migration script here
-CREATE TABLE otp_verif(
-    "user_id" uuid NOT NULL REFERENCES "users"("id") ON DELETE CASCADE,
-    "otp_code" VARCHAR(6),
-    "otp_expires_at" TIMESTAMP
-);
