615978eb0f929813c25f6296ad32bcbd61c8d90d
JiMarchel <jmarchel100@gmail.com>
2025-11-28 15:56:19 +0700

    chore: reorganize imports and refactor OTP generation logic for clarity

--- src/service/otp.rs
+++ src/service/otp.rs
@@ -1,7 +1,13 @@
+use std::sync::Arc;
+
 use async_trait::async_trait;
+use tracing::instrument;
 use uuid::Uuid;
 
-use crate::model::{app_error::AppResult, otp::OtpRecord, };
+use crate::{
+    model::{app_error::AppResult, otp::OtpRecord},
+    service::email::EmailService,
+};
 
 #[async_trait]
 pub trait OtpPersistence: Send + Sync {
@@ -21,16 +27,40 @@ pub trait OtpPersistence: Send + Sync {
     async fn delete_otp_by_user_id(&self, user_id: Uuid) -> AppResult<()>;
 }
 
-pub trait OtpGenerator: Send + Sync {
-    fn generate_otp(&self) -> String;
+fn generate_otp() -> String {
+    use rand::Rng;
+    let mut rng = rand::thread_rng();
+    format!("{:06}", rng.gen_range(0..1_000_000))
 }
 
-pub struct RandomOtpGenerator;
+pub struct OtpService {
+    pub persistence: Arc<dyn OtpPersistence>,
+    pub email_service: Arc<dyn EmailService>,
+}
 
-impl OtpGenerator for RandomOtpGenerator {
-    fn generate_otp(&self) -> String {
-        use rand::Rng;
-        let mut rng = rand::rng();
-        format!("{:06}", rng.random_range(1..1_000_000))
+impl OtpService {
+    #[instrument(
+        name="service.send_verification_otp", 
+        skip(self), 
+        fields(user_id=%user_id, email=%email, username=%username)
+    )]
+    pub async fn send_verification_otp(
+        &self,
+        user_id: Uuid,
+        email: &str,
+        username: &str,
+    ) -> AppResult<()> {
+        let otp_code = generate_otp();
+        let expires_at = chrono::Utc::now() + chrono::Duration::minutes(10);
+
+        self.persistence
+            .create_otp(user_id, &otp_code, expires_at)
+            .await?;
+
+        self.email_service
+            .send_email_async(email, username, &otp_code)
+            .await?;
+
+           Ok(())
     }
 }
