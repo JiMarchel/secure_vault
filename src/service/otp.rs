3528f481399d6589ae6eef979f3b0c98e01150a7
JiMarchel <jmarchel100@gmail.com>
2025-11-29 09:38:55 +0700

    feat: Enhance OTP service with new management methods and internal refactoring, and remove refresh token saving from user service.

--- src/service/otp.rs
+++ src/service/otp.rs
@@ -29,22 +29,31 @@ pub trait OtpPersistence: Send + Sync {
 
 fn generate_otp() -> String {
     use rand::Rng;
-    let mut rng = rand::thread_rng();
-    format!("{:06}", rng.gen_range(0..1_000_000))
+    let mut rng = rand::rng();
+    format!("{:06}", rng.random_range(0..1_000_000))
 }
 
 pub struct OtpService {
-    pub persistence: Arc<dyn OtpPersistence>,
+    pub otp_persistence: Arc<dyn OtpPersistence>,
     pub email_service: Arc<dyn EmailService>,
 }
 
 impl OtpService {
+    pub fn new(
+        otp_persistence: Arc<dyn OtpPersistence>,
+        email_service: Arc<dyn EmailService>,
+    ) -> Self {
+        Self {
+            otp_persistence,
+            email_service,
+        }
+    }
+
     #[instrument(
-        name="service.send_verification_otp", 
-        skip(self), 
-        fields(user_id=%user_id, email=%email, username=%username)
+        name = "service.send_verification_otp",
+        skip(self, user_id, email, username)
     )]
-    pub async fn send_verification_otp(
+    pub async fn send_verification(
         &self,
         user_id: Uuid,
         email: &str,
@@ -53,7 +62,7 @@ impl OtpService {
         let otp_code = generate_otp();
         let expires_at = chrono::Utc::now() + chrono::Duration::minutes(10);
 
-        self.persistence
+        self.otp_persistence
             .create_otp(user_id, &otp_code, expires_at)
             .await?;
 
@@ -61,6 +70,46 @@ impl OtpService {
             .send_email_async(email, username, &otp_code)
             .await?;
 
-           Ok(())
+        Ok(())
+    }
+
+    #[instrument(
+        name = "service.resend_verification_otp",
+        skip(self, user_id, email, username)
+    )]
+    pub async fn resend_verification(
+        &self,
+        user_id: Uuid,
+        email: &str,
+        username: &str,
+    ) -> AppResult<()> {
+        let otp_code = generate_otp();
+        let expires_at = chrono::Utc::now() + chrono::Duration::minutes(10);
+
+        self.otp_persistence
+            .update_otp_by_user_id(user_id, &otp_code, expires_at)
+            .await?;
+
+        self.email_service
+            .send_email_async(email, username, &otp_code)
+            .await?;
+
+        Ok(())
+    }
+
+    #[instrument(
+            name = "service.get_otp_by_user_id",
+        skip(self, user_id)
+    )]
+    pub async fn get_otp_by_user_id(&self, user_id: Uuid) -> AppResult<OtpRecord> {
+        Ok(self.otp_persistence.get_otp_by_user_id(user_id).await?)
+    }
+
+    #[instrument(
+        name = "service.delete_otp_by_user_id",
+        skip(self, user_id)
+    )]
+    pub async fn delete_otp_by_user_id(&self, user_id: Uuid) -> AppResult<()> {
+        self.otp_persistence.delete_otp_by_user_id(user_id).await
     }
 }
