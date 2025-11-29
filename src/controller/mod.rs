756e41b2782598a55469b0ec5b44d0e37d84c08d
JiMarchel <jmarchel100@gmail.com>
2025-11-29 09:36:38 +0700

    feat: Implement JWT-based authentication and session management, and enhance OTP service functionality.

--- src/controller/mod.rs
+++ src/controller/mod.rs
@@ -3,11 +3,15 @@ use axum::{Router, routing::get};
 use crate::controller::{app_state::AppState, health_check::health_check};
 
 pub mod app_state;
+pub mod auth;
 pub mod health_check;
+pub mod session;
 pub mod user;
 
 pub fn router() -> Router<AppState> {
     Router::new()
         .nest("/user", user::router())
+        .nest("/auth", auth::router())
+        .nest("/session", session::router())
         .route("/health", get(health_check))
 }
