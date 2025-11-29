99488144b679d13eb265c80181215af880a5ee37
JiMarchel <jmarchel100@gmail.com>
2025-11-29 09:36:14 +0700

    refactor: Extract authentication and session management into dedicated controllers and introduce JWT persistence.

--- src/controller/app_state.rs
+++ src/controller/app_state.rs
@@ -2,15 +2,29 @@ use std::sync::Arc;
 
 use axum::extract::FromRef;
 
-use crate::application::user::UserUseCase;
+use crate::application::{auth::AuthUseCase, otp::OtpUseCase, user::UserUseCase};
 
 #[derive(Clone)]
 pub struct AppState {
     pub user_use_case: Arc<UserUseCase>,
+    pub auth_use_case: Arc<AuthUseCase>,
+    pub otp_use_case: Arc<OtpUseCase>,
 }
 
 impl FromRef<AppState> for Arc<UserUseCase> {
     fn from_ref(app_state: &AppState) -> Self {
         app_state.user_use_case.clone()
+    }
+}
+
+impl FromRef<AppState> for Arc<AuthUseCase> {
+    fn from_ref(app_state: &AppState) -> Self {
+        app_state.auth_use_case.clone()
+    }
+}
+
+impl FromRef<AppState> for Arc<OtpUseCase> {
+    fn from_ref(app_state: &AppState) -> Self {
+        app_state.otp_use_case.clone()
     }
 }
