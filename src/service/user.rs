2753e406e13421eb42ae023700520ffa5aa91fd6
JiMarchel <jmarchel100@gmail.com>
2025-11-29 09:39:03 +0700

    refactor: remove save_refresh_token method from UserService trait

--- src/service/user.rs
+++ src/service/user.rs
@@ -17,7 +17,4 @@ pub trait UserPersistence: Send + Sync {
         argon2_params: String,
         user_id: Uuid,
     ) -> AppResult<()>;
-
-    //Jwt things
-    async fn save_refresh_token(&self, user_id: Uuid, refresh_token: &str) -> AppResult<()>;
 }
