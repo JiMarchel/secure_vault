8dd6f6a7336bf8a3f0edb4de5b0767f13276b309
JiMarchel <jmarchel100@gmail.com>
2025-11-30 18:43:23 +0700

    feat: Introduce telemetry infrastructure and API test helpers, including a fake email service.

---
+++ src/infra/telemetry.rs
@@ -0,0 +1,26 @@
+use tracing::{Subscriber, subscriber::set_global_default};
+use tracing_subscriber::{
+    EnvFilter, Registry,
+    fmt::{self, MakeWriter},
+    layer::SubscriberExt,
+};
+
+pub fn get_subscriber<Sink>(env_filter: String, sink: Sink) -> impl Subscriber + Send + Sync
+where
+    Sink: for<'a> MakeWriter<'a> + Send + Sync + 'static,
+{
+    let filter = EnvFilter::try_from_default_env().unwrap_or_else(|_| EnvFilter::new(env_filter));
+
+    let console_layer = fmt::layer()
+        .with_target(true)
+        .with_level(true)
+        .json()
+        .with_writer(sink);
+
+    Registry::default().with(filter).with(console_layer)
+}
+
+pub fn init_subscriber(subscriber: impl Subscriber + Send + Sync) {
+    // Redirect all Log events into the subscriber
+    set_global_default(subscriber).expect("Failed to set subscriber");
+}
