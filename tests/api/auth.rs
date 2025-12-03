415832620b6363c92d4ce4aabe0862bfd04c8479
JiMarchel <jmarchel100@gmail.com>
2025-12-03 19:52:23 +0700

    fix: update test case for sign-up to use consistent example data

--- tests/api/auth.rs
+++ tests/api/auth.rs
@@ -4,7 +4,13 @@ use crate::helpers::spawn_app;
 async fn test_sign_up_sends_otp_email() {
     let app = spawn_app().await;
 
-    let response = app.sign_up("wkwk".into(), "jmarchel200@gmail.com".into()).await;
+    let response = app.sign_up("example".into(), "example@gmail.com".into()).await;
 
-    assert_eq!(response.status(), 200)
+    assert_eq!(response.status(), 200);
+
+    assert!(app.email_service.was_email_sent_to("example@gmail.com"));
+    let otp = app.email_service.get_otp_for_email("example@gmail.com");
+    assert!(otp.is_some());
+    assert_eq!(otp.unwrap().len(), 6);
 }
+
