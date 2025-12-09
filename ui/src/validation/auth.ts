2af576dd7558fd92e339218fcf3e95c85b25f49c
JiMarchel <jmarchel100@gmail.com>
2025-12-09 08:27:00 +0700

    feat: add validation for OTP and password fields with enhanced rules

--- ui/src/validation/auth.ts
+++ ui/src/validation/auth.ts
@@ -1,4 +1,5 @@
-import z from 'zod'
+import { z } from 'zod'
+import type { RefinementCtx } from 'zod'
 
 export const signUp = z.object({
   username: z.string().min(3).max(256, 'Username is too long'),
@@ -9,3 +10,46 @@ export const signUp = z.object({
       'Sorry for now just support gmail address',
     ),
 })
+
+export const verifOtp = z.object({
+  otp_code: z.string().length(6, "OTP must be exactly 6 digits long."),
+});
+
+export const verifPassword = z
+  .object({
+    password: z
+      .string()
+      .min(8, 'Password must be at least 8 characters long.')
+      .max(100, 'Password must be at most 100 characters long.')
+      .refine((val: string) => /[A-Z]/.test(val), {
+        message: 'Password must contain at least one uppercase letter.',
+      })
+      .refine((val: string) => /[a-z]/.test(val), {
+        message: 'Password must contain at least one lowercase letter.',
+      })
+      .refine((val: string) => /\d/.test(val), {
+        message: 'Password must contain at least one number.',
+      })
+      .refine((val: string) => /[!@#$%^&*(),.?":{}|<>]/.test(val), {
+        message: 'Password must contain at least one special character.',
+      }),
+    confirmPassword: z
+      .string()
+      .min(8, 'Confirm password must be at least 8 characters long.')
+      .max(100, 'Confirm password must be at most 100 characters long.'),
+  })
+  .superRefine(
+    (
+      data: { password: string; confirmPassword: string },
+      ctx: RefinementCtx,
+    ) => {
+      if (data.confirmPassword !== data.password) {
+        ctx.addIssue({
+          code: 'custom',
+          path: ['confirmPassword'],
+          message: 'Passwords do not match.',
+        })
+      }
+    },
+  )
+  .strict()
