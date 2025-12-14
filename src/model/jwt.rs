a5fbecea8749b153ed0ae3b45d2eeaa51efd45e0
JiMarchel <jmarchel100@gmail.com>
2025-12-14 19:06:36 +0700

    feat: Update Claims struct to use Uuid for subject and remove unused fields from AuthTokens

--- src/model/jwt.rs
+++ src/model/jwt.rs
@@ -1,9 +1,10 @@
 use serde::{Deserialize, Serialize};
+use uuid::Uuid;
 
 #[derive(Serialize, Deserialize, Clone)]
 #[serde(rename_all = "camelCase")]
 pub struct Claims {
-    pub sub: String,
+    pub sub: Uuid,
     pub exp: i64,
     pub iat: i64,
     pub email: String,
@@ -14,7 +15,4 @@ pub struct Claims {
 pub struct AuthTokens {
     pub access_token: String,
     pub refresh_token: String,
-    pub token_type: String,
-    /// seconds
-    pub expires_in: i64,
 }
