7e05acf712a7c44ad810aaf1e01997eb5bcc5a07
JiMarchel <jmarchel100@gmail.com>
2026-01-10 16:01:51 +0700

    feat: Add DashboardNavbar component for user authentication and logout functionality

---
+++ ui/app/components/DashboardNavbar.vue
@@ -0,0 +1,22 @@
+<script lang="ts" setup>
+import type { Button } from './ui/button';
+import { useAuth } from '~/composable/auth';
+import { Spinner } from './ui/spinner';
+
+const {logout, isLoading} = useAuth()
+
+</script>
+
+<template>
+      <header class="sticky top-0 z-50 w-full border-b py-2">
+        <div class="flex justify-between mx-8 ">
+            <Button>
+                wk
+            </Button>
+            <Button :disabled="isLoading" class="cursor-pointer" @click="logout">
+                <Spinner v-if="isLoading"/>
+                Logout
+            </Button>
+        </div>
+    </header>
+</template>
