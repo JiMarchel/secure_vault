5049e26d2f64692ce154863375eca5c6b747fc94
JiMarchel <jmarchel100@gmail.com>
2026-01-11 16:41:26 +0700

    feat: new query for get auth verifier

--- src/persistence/user.rs
+++ src/persistence/user.rs
@@ -77,7 +77,7 @@ impl UserPersistence for PostgresPersistence {
     )]
     async fn get_user_identifier(&self, email: &str) -> AppResult<Option<UserIdentifier>> {
         Ok(sqlx::query_as::<_, UserIdentifier>(
-            "SELECT encrypted_dek, nonce, salt, argon2_params FROM users WHERE email = $1",
+            "SELECT encrypted_dek, nonce, salt, argon2_params, auth_verifier FROM users WHERE email = $1",
         )
         .bind(email)
         .fetch_optional(&self.pool)
@@ -86,7 +86,7 @@ impl UserPersistence for PostgresPersistence {
 
     #[instrument(
         name= "persistence.update_user_identifier",
-        skip(self, encrypted_dek, nonce, salt, argon2_params),
+        skip(self, encrypted_dek, nonce, salt, argon2_params, auth_verifier),
         fields(user_id=%user_id)
     )]
     async fn update_user_identifier(
@@ -95,20 +95,36 @@ impl UserPersistence for PostgresPersistence {
         nonce: String,
         salt: String,
         argon2_params: String,
+        auth_verifier: String,
         user_id: Uuid,
     ) -> AppResult<()> {
         sqlx::query(
-            "UPDATE users SET encrypted_dek = $1, nonce = $2, salt = $3, argon2_params = $4 WHERE id = $5",
+            "UPDATE users SET encrypted_dek = $1, nonce = $2, salt = $3, argon2_params = $4, auth_verifier = $5 WHERE id = $6",
         )
         .bind(encrypted_dek)
         .bind(nonce)
         .bind(salt)
         .bind(argon2_params)
+        .bind(auth_verifier)
         .bind(user_id)
         .execute(&self.pool)
         .await?;
 
         Ok(())
+    }
+
+    #[instrument(
+        name= "persistence.get_auth_verifier_by_email",
+        skip(self),
+        fields(email=%email)
+    )]
+    async fn get_auth_verifier_by_email(&self, email: &str) -> AppResult<Option<String>> {
+        Ok(
+            sqlx::query_scalar("SELECT auth_verifier FROM users WHERE email = $1")
+                .bind(email)
+                .fetch_optional(&self.pool)
+                .await?,
+        )
     }
 
     #[instrument(
