67516010f8be7811fbe25af50dd35022891a60c9
JiMarchel <jmarchel100@gmail.com>
2026-01-19 22:15:11 +0700

    feat: replace Dialog component with FormDialog for login and signup forms

--- ui/app/components/HomeNavbar.vue
+++ ui/app/components/HomeNavbar.vue
@@ -1,6 +1,5 @@
 <script setup lang="ts">
 import { Shield } from 'lucide-vue-next';
-import { Dialog, DialogContent, DialogDescription, DialogHeader, DialogTitle, DialogTrigger } from './ui/dialog';
 import { Button } from './ui/button';
 import { useForm } from '@tanstack/vue-form'
 import { login as loginValidation, signUp } from '~/utils/validation/auth';
@@ -10,11 +9,11 @@ import type { signUpType } from '~/utils/model/auth';
 import { errorHelper } from '~/lib/error-helper';
 import type { SuccessResponse } from '~/utils/model/response';
 import { toast } from 'vue-sonner';
-import { useAuth } from '~/composable/auth';
 import { Spinner } from './ui/spinner';
+import FormDialog from './FormDialog.vue';
 
 const config = useRuntimeConfig();
-const {login, isLoading} = useAuth()
+const { login, isLoading } = useAuth()
 const signUpForm = useForm({
     defaultValues: {
         username: "",
@@ -79,121 +78,92 @@ function isInvalid(field: any) {
             </NuxtLink>
 
             <div class="flex items-center space-x-3">
-                <Dialog>
-                    <DialogTrigger as-child>
+                <FormDialog title="Welcome back" description="Sign in to your SecureVault account" submit-text="Sign In"
+                    :loading="isLoading" @submit="loginForm.handleSubmit">
+                    <template #trigger>
                         <Button variant="ghost" size="sm">
                             Sign In
                         </Button>
-                    </DialogTrigger>
-                    <DialogContent class="sm:max-w-md">
-                        <DialogHeader>
-                            <DialogTitle>Welcome back</DialogTitle>
-                            <DialogDescription>
-                                Sign in to your SecureVault account
-                            </DialogDescription>
-                        </DialogHeader>
-                        <form class="space-y-4" @submit.prevent="loginForm.handleSubmit">
-                            <FieldGroup>
-                                <loginForm.Field name="email">
-                                    <template #default="{ field }">
-                                        <Field :data-invalid="isInvalid(field)">
-                                            <FieldLabel :for="field.name">
-                                                Email
-                                            </FieldLabel>
-                                            <Input
-:id="field.name" :name="field.name" :model-value="field.state.value"
-                                                :aria-invalid="isInvalid(field)" placeholder="Your email here..."
-                                                autocomplete="off" @blur="field.handleBlur"
-                                                @input="field.handleChange($event.target.value)" />
-                                            <FieldError v-if="isInvalid(field)" :errors="field.state.meta.errors" />
-                                        </Field>
-                                    </template>
-                                </loginForm.Field>
-                                <loginForm.Field name="password">
-                                    <template #default="{ field }">
-                                        <Field :data-invalid="isInvalid(field)">
-                                            <FieldLabel :for="field.name">
-                                                Password
-                                            </FieldLabel>
-                                            <Input
-:id="field.name" :name="field.name" :model-value="field.state.value"
-                                                :aria-invalid="isInvalid(field)" placeholder="Your password here..."
-                                                autocomplete="off" type="password"
-                                                @blur="field.handleBlur" @input="field.handleChange($event.target.value)"/>
-                                            <FieldError v-if="isInvalid(field)" :errors="field.state.meta.errors" />
-                                        </Field>
-                                    </template>
-                                </loginForm.Field>
-                            </FieldGroup>
+                    </template>
 
-                            <div class="flex items-center justify-between">
-                                <NuxtLink href="#" class="text-sm text-primary hover:text-primary/70">
-                                    Forgot password?
-                                </NuxtLink>
-                            </div>
-                            <Button type="submit" class="w-full bg-primary hover:bg-primary/70" :disabled="isLoading">
-                                <Spinner v-if="isLoading"/>
-                                Sign In
-                            </Button>
-                        </form>
-                    </DialogContent>
-                </Dialog>
+                    <FieldGroup>
+                        <loginForm.Field name="email">
+                            <template #default="{ field }">
+                                <Field :data-invalid="isInvalid(field)">
+                                    <FieldLabel :for="field.name">
+                                        Email
+                                    </FieldLabel>
+                                    <Input :id="field.name" :name="field.name" :model-value="field.state.value"
+                                        :aria-invalid="isInvalid(field)" placeholder="Your email here..."
+                                        autocomplete="off" @blur="field.handleBlur"
+                                        @input="field.handleChange($event.target.value)" />
+                                    <FieldError v-if="isInvalid(field)" :errors="field.state.meta.errors" />
+                                </Field>
+                            </template>
+                        </loginForm.Field>
+                        <loginForm.Field name="password">
+                            <template #default="{ field }">
+                                <Field :data-invalid="isInvalid(field)">
+                                    <FieldLabel :for="field.name">
+                                        Password
+                                    </FieldLabel>
+                                    <Input :id="field.name" :name="field.name" :model-value="field.state.value"
+                                        :aria-invalid="isInvalid(field)" placeholder="Your password here..."
+                                        autocomplete="off" type="password" @blur="field.handleBlur"
+                                        @input="field.handleChange($event.target.value)" />
+                                    <FieldError v-if="isInvalid(field)" :errors="field.state.meta.errors" />
+                                </Field>
+                            </template>
+                        </loginForm.Field>
+                    </FieldGroup>
 
-                <Dialog>
-                    <DialogTrigger as-child>
+                    <div class="flex items-center justify-between">
+                        <NuxtLink href="#" class="text-sm text-primary hover:text-primary/70">
+                            Forgot password?
+                        </NuxtLink>
+                    </div>
+                </FormDialog>
+
+                <FormDialog title="Create your account" description="Start securing your passwords today"
+                    submit-text="Submit" :loading="isLoading" @submit="signUpForm.handleSubmit">
+                    <template #trigger>
                         <Button size="sm" class="bg-primary hover:bg-primary/70 cursor-pointer">
                             Get Started
                         </Button>
-                    </DialogTrigger>
-                    <DialogContent class="sm:max-w-md">
-                        <DialogHeader>
-                            <DialogTitle>Create your account</DialogTitle>
-                            <DialogDescription>
-                                Start securing your passwords today
-                            </DialogDescription>
-                        </DialogHeader>
-                        <form class="space-y-4" @submit.prevent="signUpForm.handleSubmit">
-                            <FieldGroup>
-                                <signUpForm.Field name="username">
-                                    <template #default="{ field }">
-                                        <Field :data-invalid="isInvalid(field)">
-                                            <FieldLabel :for="field.name">
-                                                Username
-                                            </FieldLabel>
-                                            <Input
-:id="field.name" :name="field.name" :model-value="field.state.value"
-                                                :aria-invalid="isInvalid(field)" placeholder="Your username here..."
-                                                autocomplete="off" @blur="field.handleBlur"
-                                                @input="field.handleChange($event.target.value)" />
-                                            <FieldError v-if="isInvalid(field)" :errors="field.state.meta.errors" />
-                                        </Field>
-                                    </template>
-                                </signUpForm.Field>
-                                <signUpForm.Field name="email">
-                                    <template #default="{ field }">
-                                        <Field :data-invalid="isInvalid(field)">
-                                            <FieldLabel :for="field.name">
-                                                Email
-                                            </FieldLabel>
-                                            <Input
-:id="field.name" :name="field.name" :model-value="field.state.value"
-                                                :aria-invalid="isInvalid(field)" placeholder="Your email here..."
-                                                autocomplete="off" @blur="field.handleBlur"
-                                                @input="field.handleChange($event.target.value)" />
-                                            <FieldError v-if="isInvalid(field)" :errors="field.state.meta.errors" />
-                                        </Field>
-                                    </template>
-                                </signUpForm.Field>
-                            </FieldGroup>
-                            <Button type="submit" :disabled="isLoading">
-                                <Spinner v-if="isLoading"/>
-                                Submit
-                            </Button>
-                        </form>
-                    </DialogContent>
-                </Dialog>
+                    </template>
+
+                    <FieldGroup>
+                        <signUpForm.Field name="username">
+                            <template #default="{ field }">
+                                <Field :data-invalid="isInvalid(field)">
+                                    <FieldLabel :for="field.name">
+                                        Username
+                                    </FieldLabel>
+                                    <Input :id="field.name" :name="field.name" :model-value="field.state.value"
+                                        :aria-invalid="isInvalid(field)" placeholder="Your username here..."
+                                        autocomplete="off" @blur="field.handleBlur"
+                                        @input="field.handleChange($event.target.value)" />
+                                    <FieldError v-if="isInvalid(field)" :errors="field.state.meta.errors" />
+                                </Field>
+                            </template>
+                        </signUpForm.Field>
+                        <signUpForm.Field name="email">
+                            <template #default="{ field }">
+                                <Field :data-invalid="isInvalid(field)">
+                                    <FieldLabel :for="field.name">
+                                        Email
+                                    </FieldLabel>
+                                    <Input :id="field.name" :name="field.name" :model-value="field.state.value"
+                                        :aria-invalid="isInvalid(field)" placeholder="Your email here..."
+                                        autocomplete="off" @blur="field.handleBlur"
+                                        @input="field.handleChange($event.target.value)" />
+                                    <FieldError v-if="isInvalid(field)" :errors="field.state.meta.errors" />
+                                </Field>
+                            </template>
+                        </signUpForm.Field>
+                    </FieldGroup>
+                </FormDialog>
             </div>
         </div>
     </header>
-
 </template>
