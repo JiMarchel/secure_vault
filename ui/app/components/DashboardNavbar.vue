4be54f1c8a2c13bb11095a179677b3fd4546910d
JiMarchel <jmarchel100@gmail.com>
2026-01-19 22:14:57 +0700

    feat: enhance DashboardNavbar to display username with fallback spinner

--- ui/app/components/DashboardNavbar.vue
+++ ui/app/components/DashboardNavbar.vue
@@ -1,20 +1,24 @@
 <script lang="ts" setup>
 import type { Button } from './ui/button';
-import { useAuth } from '~/composable/auth';
 import { Spinner } from './ui/spinner';
 
-const {logout, isLoading} = useAuth()
+const { logout, isLoading, user } = useAuth()
 
 </script>
 
 <template>
-      <header class="sticky top-0 z-50 w-full border-b py-2">
+    <header class="sticky top-0 z-50 w-full border-b py-2">
         <div class="flex justify-between mx-8 ">
             <Button>
-                wk
+                <ClientOnly>
+                    {{ user?.username }}
+                    <template #fallback>
+                        <Spinner />
+                    </template>
+                </ClientOnly>
             </Button>
             <Button :disabled="isLoading" class="cursor-pointer" @click="logout">
-                <Spinner v-if="isLoading"/>
+                <Spinner v-if="isLoading" />
                 Logout
             </Button>
         </div>
