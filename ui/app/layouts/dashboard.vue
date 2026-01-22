208f000665af73f9217d74a5cb1b434e2abca75b
JiMarchel <jmarchel100@gmail.com>
2026-01-22 11:57:02 +0700

    chore: wrap modal to client only

--- ui/app/layouts/dashboard.vue
+++ ui/app/layouts/dashboard.vue
@@ -1,9 +1,18 @@
 <script setup lang="ts">
 import AppSidebar from '~/components/AppSidebar.vue';
 import { SidebarProvider, SidebarTrigger } from '~/components/ui/sidebar';
+import UnlockVaultModal from '~/components/UnlockVaultModal.vue';
+import { errorHelper } from '~/lib/error-helper';
 
+const { needsUnlock, user, unlockVault, logout } = useAuth();
 
-
+async function handleUnlock(password: string) {
+    try {
+        await unlockVault(password);
+    } catch (error: any) {
+        await errorHelper(error)
+    }
+}
 </script>
 
 <template>
@@ -12,6 +21,10 @@ import { SidebarProvider, SidebarTrigger } from '~/components/ui/sidebar';
         <div>
             <SidebarTrigger />
             <slot />
+            <ClientOnly>
+                <UnlockVaultModal v-if="needsUnlock" :userEmail="user?.email!" @unlock="handleUnlock"
+                    @logout="logout" />
+            </ClientOnly>
         </div>
     </SidebarProvider>
 </template>
