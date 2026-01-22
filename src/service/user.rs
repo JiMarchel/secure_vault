9f47c7286037d6916aac4b86741f82e48d9a8b73
JiMarchel <jmarchel100@gmail.com>
2026-01-22 18:51:13 +0700

    refactor: rename some function to make it readable

--- src/service/user.rs
+++ src/service/user.rs
@@ -3,16 +3,16 @@ use uuid::Uuid;
 
 use crate::model::{
     app_error::AppResult,
-    user::{User, UserIdentifier, UserInfo},
+    user::{User, PublicUser, UserIdentifier},
 };
 
 #[async_trait]
 pub trait UserPersistence: Send + Sync {
-    async fn create_user(&self, username: &str, email: &str) -> AppResult<Uuid>;
-    async fn get_user_by_email(&self, email: &str) -> AppResult<Option<User>>;
-    async fn get_user_by_id(&self, id: Uuid) -> AppResult<Option<User>>;
-    async fn update_email_verification(&self, id: Uuid) -> AppResult<()>;
-    async fn update_user_identifier(
+    async fn insert(&self, username: &str, email: &str) -> AppResult<Uuid>;
+    async fn find_by_email(&self, email: &str) -> AppResult<Option<User>>;
+    async fn find_by_id(&self, id: Uuid) -> AppResult<Option<User>>;
+    async fn update_email_verified_by_id(&self, id: Uuid) -> AppResult<()>;
+    async fn update_identifier_by_id(
         &self,
         encrypted_dek: String,
         nonce: String,
@@ -21,8 +21,8 @@ pub trait UserPersistence: Send + Sync {
         auth_verifier: String,
         user_id: Uuid,
     ) -> AppResult<()>;
-    async fn get_auth_verifier_by_email(&self, email: &str) -> AppResult<Option<String>>;
-    async fn get_user_identifier(&self, email: &str) -> AppResult<Option<UserIdentifier>>;
-    async fn get_user_info_by_email(&self, email: &str) -> AppResult<Option<UserInfo>>;
-    async fn get_user_info_by_id(&self, id: Uuid) -> AppResult<Option<UserInfo>>;
+    async fn find_verifier_by_email(&self, email: &str) -> AppResult<Option<String>>;
+    async fn find_identifier_by_email(&self, email: &str) -> AppResult<Option<UserIdentifier>>;
+    async fn find_public_user_by_email(&self, email: &str) -> AppResult<Option<PublicUser>>;
+    async fn find_public_user_by_id(&self, id: Uuid) -> AppResult<Option<PublicUser>>;
 }
