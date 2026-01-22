8098f3324b9f8a8e2d021c87eed9a704c3ba81ad
JiMarchel <jmarchel100@gmail.com>
2026-01-22 18:52:46 +0700

    refactor: rename user-related methods for consistency and clarity

--- src/persistence/postgres/user.rs
+++ src/persistence/postgres/user.rs
@@ -5,7 +5,7 @@ use uuid::Uuid;
 use crate::{
     model::{
         app_error::AppResult,
-        user::{User, UserIdentifier, UserInfo},
+        user::{PublicUser, User, UserIdentifier},
     },
     persistence::postgres::PostgresPersistence,
     service::user::UserPersistence,
@@ -14,11 +14,11 @@ use crate::{
 #[async_trait]
 impl UserPersistence for PostgresPersistence {
     #[instrument(
-        name= "persistence.create_user",
+        name= "persistence.user.insert",
         skip(self),
         fields(email=%email, username=%username)
     )]
-    async fn create_user(&self, username: &str, email: &str) -> AppResult<Uuid> {
+    async fn insert(&self, username: &str, email: &str) -> AppResult<Uuid> {
         Ok(
             sqlx::query_scalar("INSERT INTO users (username, email) VALUES($1, $2) RETURNING id")
                 .bind(username)
@@ -29,11 +29,11 @@ impl UserPersistence for PostgresPersistence {
     }
 
     #[instrument(
-        name= "persistence.get_user_by_email",
+        name= "persistence.user.find_by_email",
         skip(self),
         fields(email=%email)
     )]
-    async fn get_user_by_email(&self, email: &str) -> AppResult<Option<User>> {
+    async fn find_by_email(&self, email: &str) -> AppResult<Option<User>> {
         Ok(
             sqlx::query_as::<_, User>("SELECT * FROM users WHERE email = $1")
                 .bind(email)
@@ -43,11 +43,11 @@ impl UserPersistence for PostgresPersistence {
     }
 
     #[instrument(
-        name= "persistence.get_user_by_id",
+        name= "persistence.user.find_by_id",
         skip(self),
         fields(user_id=%id)
     )]
-    async fn get_user_by_id(&self, id: Uuid) -> AppResult<Option<User>> {
+    async fn find_by_id(&self, id: Uuid) -> AppResult<Option<User>> {
         Ok(
             sqlx::query_as::<_, User>("SELECT * FROM users WHERE id = $1")
                 .bind(id)
@@ -57,11 +57,11 @@ impl UserPersistence for PostgresPersistence {
     }
 
     #[instrument(
-        name= "persistence.update_email_verification",
+        name= "persistence.user.update_email_verified_by_id",
         skip(self),
         fields(user_id=%id)
     )]
-    async fn update_email_verification(&self, id: Uuid) -> AppResult<()> {
+    async fn update_email_verified_by_id(&self, id: Uuid) -> AppResult<()> {
         sqlx::query("UPDATE users SET is_email_verified = true WHERE id = $1")
             .bind(id)
             .execute(&self.pool)
@@ -71,11 +71,11 @@ impl UserPersistence for PostgresPersistence {
     }
 
     #[instrument(
-        name= "persistence.get_user_identifier",
+        name= "persistence.user.find_identifier_by_email",
         skip(self),
         fields(email=%email)
     )]
-    async fn get_user_identifier(&self, email: &str) -> AppResult<Option<UserIdentifier>> {
+    async fn find_identifier_by_email(&self, email: &str) -> AppResult<Option<UserIdentifier>> {
         Ok(sqlx::query_as::<_, UserIdentifier>(
             "SELECT encrypted_dek, nonce, salt, argon2_params, auth_verifier FROM users WHERE email = $1",
         )
@@ -85,11 +85,11 @@ impl UserPersistence for PostgresPersistence {
     }
 
     #[instrument(
-        name= "persistence.update_user_identifier",
+        name= "persistence.user.update_identifier_by_id",
         skip(self, encrypted_dek, nonce, salt, argon2_params, auth_verifier),
         fields(user_id=%user_id)
     )]
-    async fn update_user_identifier(
+    async fn update_identifier_by_id(
         &self,
         encrypted_dek: String,
         nonce: String,
@@ -114,11 +114,11 @@ impl UserPersistence for PostgresPersistence {
     }
 
     #[instrument(
-        name= "persistence.get_auth_verifier_by_email",
+        name= "persistence.user.find_verifier_by_email",
         skip(self),
         fields(email=%email)
     )]
-    async fn get_auth_verifier_by_email(&self, email: &str) -> AppResult<Option<String>> {
+    async fn find_verifier_by_email(&self, email: &str) -> AppResult<Option<String>> {
         Ok(
             sqlx::query_scalar("SELECT auth_verifier FROM users WHERE email = $1")
                 .bind(email)
@@ -128,27 +128,27 @@ impl UserPersistence for PostgresPersistence {
     }
 
     #[instrument(
-        name= "persistence.get_user_info_by_email",
+        name= "persistence.user.find_public_user_by_email",
         skip(self),
         fields(email=%email)
     )]
-    async fn get_user_info_by_email(&self, email: &str) -> AppResult<Option<UserInfo>> {
-        Ok(
-            sqlx::query_as::<_, UserInfo>("SELECT id, email, username FROM users WHERE email = $1")
-                .bind(email)
-                .fetch_optional(&self.pool)
-                .await?,
+    async fn find_public_user_by_email(&self, email: &str) -> AppResult<Option<PublicUser>> {
+        Ok(sqlx::query_as::<_, PublicUser>(
+            "SELECT id, email, username FROM users WHERE email = $1",
         )
+        .bind(email)
+        .fetch_optional(&self.pool)
+        .await?)
     }
 
     #[instrument(
-        name= "persistence.get_user_info_by_id",
+        name= "persistence.user.find_public_user_by_id",
         skip(self),
         fields(user_id=%id)
     )]
-    async fn get_user_info_by_id(&self, id: Uuid) -> AppResult<Option<UserInfo>> {
+    async fn find_public_user_by_id(&self, id: Uuid) -> AppResult<Option<PublicUser>> {
         Ok(
-            sqlx::query_as::<_, UserInfo>("SELECT id, email, username FROM users WHERE id = $1")
+            sqlx::query_as::<_, PublicUser>("SELECT id, email, username FROM users WHERE id = $1")
                 .bind(id)
                 .fetch_optional(&self.pool)
                 .await?,
