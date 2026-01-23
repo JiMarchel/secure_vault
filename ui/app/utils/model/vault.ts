454a64ba16c9ffbb7df21e846dbdac4ba5871ada
JiMarchel <jmarchel100@gmail.com>
2026-01-23 10:02:22 +0700

    chore: create vaults interface

---
+++ ui/app/utils/model/vault.ts
@@ -0,0 +1,9 @@
+export interface vaults {
+    id: string;
+    title: string;
+    encryptedData: string;
+    nonce: string;
+    itemType: string;
+    createdAt: string;
+    updatedAt: string;
+}
