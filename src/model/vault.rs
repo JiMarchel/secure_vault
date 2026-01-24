eaaee8c807d4d79e9b34b21599b9588ceda4c84c
JiMarchel <jmarchel100@gmail.com>
2026-01-24 08:59:49 +0700

    feat: add update vault model

--- src/model/vault.rs
+++ src/model/vault.rs
@@ -18,6 +18,16 @@ pub struct Vaults {
 
 #[derive(Deserialize)]
 #[serde(rename_all = "camelCase")]
+pub struct UpdateVaultRequest {
+    pub id: Uuid,
+    pub title: String,
+    pub item_type: ItemType,
+    pub encrypted_data: String,
+    pub nonce: String,
+}
+
+#[derive(Deserialize)]
+#[serde(rename_all = "camelCase")]
 pub struct VaultRequest {
     pub title: String,
     pub item_type: ItemType,
