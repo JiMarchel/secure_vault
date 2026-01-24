d87b230b3cea1073b5598e6456f169260e6cde3a
JiMarchel <jmarchel100@gmail.com>
2026-01-24 11:12:19 +0700

    feat: vault composable

--- ui/app/composables/use-vault.ts
+++ ui/app/composables/use-vault.ts
@@ -1,10 +1,20 @@
+import { toast } from "vue-sonner";
+import type { VaultItem } from "~/lib/wasm/type";
+import { decryptVaultItem, encryptVaultItem } from "~/lib/wasm/vault";
 import type { SuccessResponse } from "~/utils/model/response";
-import type { Vaults } from "~/utils/model/vault";
+import type { EncryptedVault, Vaults } from "~/utils/model/vault";
+import { addPassword, type addPasswordType } from "~/utils/validation/vaults";
 
 export const useVaults = () => {
+  const isLoading = ref(false);
   const headers = useRequestHeaders(["cookie"]);
+  const { useDek } = useAuth();
 
-  const { data: vaults, refresh: refreshVaults, error: errorVaults } = useAsyncData(
+  const {
+    data: vaults,
+    refresh: refreshVaults,
+    error: errorVaults,
+  } = useAsyncData(
     "vaults",
     async () => {
       const res = await $fetch<SuccessResponse<Vaults[]>>("/vault/all", {
@@ -20,9 +30,100 @@ export const useVaults = () => {
     },
   );
 
+  const encryptVault = async (value: string) => {
+    const dek = useDek();
+    const encryptedVaultRes = await encryptVaultItem(dek, value);
+    return encryptedVaultRes;
+  };
+
+  const decryptVault = async (value: VaultItem) => {
+    const dek = useDek();
+    const decryptedVaultRes = await decryptVaultItem(dek, value);
+    return decryptedVaultRes;
+  };
+
+  const add = async (body: EncryptedVault) => {
+    isLoading.value = true;
+
+    const { $api } = useNuxtApp();
+
+    const res = await $api<SuccessResponse<void>>("/vault", {
+      body,
+      method: "POST",
+    });
+
+    toast.success(res.message);
+
+    await refreshVaults();
+  }
+
+  const deleteVault = async (id: string) => {
+    isLoading.value = true;
+
+    const { $api } = useNuxtApp();
+
+    const res = await $api<SuccessResponse<void>>(`/vault/${id}`, {
+      method: "DELETE",
+    });
+
+    toast.success(res.message);
+
+    await refreshVaults();
+
+    isLoading.value = false;
+  }
+
+  const updatePassword = async (body: EncryptedVault) => {
+    isLoading.value = true;
+
+    const { $api } = useNuxtApp();
+
+    const res = await $api<SuccessResponse<void>>(`/vault`, {
+      body,
+      method: "PUT",
+    });
+
+    toast.success(res.message);
+
+    await refreshVaults();
+  }
+
+  const password = async (value: addPasswordType, update?: boolean, id?: string) => {
+    const credsValue = {
+      usernameOrEmail: value.usernameOrEmail,
+      password: value.password,
+      websiteOrApp: value.websiteOrApp,
+    };
+
+    const encryptedVaultRes = await encryptVault(JSON.stringify(credsValue));
+    
+    if (update) {
+      const bodyReq = {
+        id,
+        title: value.title,
+        itemType: "Password",
+        ...encryptedVaultRes,
+      };
+      await updatePassword(bodyReq);
+    } else {
+      const bodyReq = {
+        title: value.title,
+        itemType: "Password",
+        ...encryptedVaultRes,
+      };
+      await add(bodyReq);
+    }
+    isLoading.value = false;
+  };
+
   return {
     vaults,
     refreshVaults,
-    errorVaults
+    errorVaults,
+
+    isLoading,
+
+    password,
+    deleteVault,
   };
 };
