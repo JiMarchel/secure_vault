131e1668731ed24d66ddfe756aa9033882dc1775
JiMarchel <jmarchel100@gmail.com>
2026-01-24 19:51:06 +0700

    feat: add search_by_title method to VaultPersistence trait for title-based vault retrieval

--- src/service/vault.rs
+++ src/service/vault.rs
@@ -19,4 +19,5 @@ pub trait VaultPersistence: Send + Sync {
     async fn find_all_by_user_id(&self, user_id: Uuid) -> AppResult<Vec<Vaults>>;
     async fn update(&self, user_id: Uuid, vault: UpdateVaultRequest) -> AppResult<()>;
     async fn delete(&self, user_id: Uuid, id: Uuid) -> AppResult<()>;
+    async fn search_by_title(&self, user_id: Uuid, title: String) -> AppResult<Vec<Vaults>>;
 }
